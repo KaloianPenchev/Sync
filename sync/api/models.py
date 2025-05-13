a134aa9fdba666345099001b4704daa91cc4cd54
Kaloian Penchev <kaloanpencev8@gmail.com>
2025-05-13 13:56:43 +0300

    created User model

--- sync/api/models.py
+++ sync/api/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    username = models.CharField(max_length=100, unique=True)
+    email = models.EmailField(unique=True)
+    password = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
