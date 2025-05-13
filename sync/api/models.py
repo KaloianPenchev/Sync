021afcf6f7439cc1a7c45140b24342e6799367a8
Kaloian Penchev <kaloanpencev8@gmail.com>
2025-05-13 13:58:07 +0300

    created Post model

--- sync/api/models.py
+++ sync/api/models.py
@@ -6,3 +6,15 @@ class User(models.Model):
     password = models.CharField(max_length=100)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+class Post(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    likes_count = models.IntegerField(default=0)
+
+
+    
+    
