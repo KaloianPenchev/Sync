ab0b78aef47a39e185f518c20f625d7d39e7c2c2
Kaloian Penchev <kaloanpencev8@gmail.com>
2025-05-15 11:18:36 +0300

    implemented serializers

---
+++ sync/api/serializers.py
@@ -0,0 +1,29 @@
+from rest_framework import serializers
+from .models import User, Post, Comment, Like
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'email', 'created_at']
+        read_only_fields = ['created_at']
+
+class PostSerializer(serializers.ModelSerializer):
+    user = UserSerializer(read_only=True)
+    
+    class Meta:
+        model = Post
+        fields = ['id', 'user', 'title', 'content', 'created_at', 'updated_at', 'likes_count']
+        read_only_fields = ['created_at', 'updated_at', 'likes_count', 'user']
+    
+    def create(self, validated_data):
+        user = self.context['request'].user_id
+        post = Post.objects.create(user_id=user, **validated_data)
+        return post
+
+class CommentSerializer(serializers.ModelSerializer):
+    user = UserSerializer(read_only=True)
+    
+    class Meta:
+        model = Comment
+        fields = ['id', 'user', 'post', 'content', 'created_at']
+        read_only_fields = ['created_at', 'user'] 
