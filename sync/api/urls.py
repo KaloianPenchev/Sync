bc15e7fc0211418cb50917d625da4ddea2b202ed
Kaloian Penchev <kaloanpencev8@gmail.com>
2025-05-16 14:27:42 +0300

    created url for the posts view

--- sync/api/urls.py
+++ sync/api/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path(""),
+    path("posts/", views.post_list, name="post-list"),
+    path("posts/<int:pk>/", views.post_detail, name="post-detail"),
 ]
