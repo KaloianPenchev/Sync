7a0ee73e5ced1b6b8eedd929e8ab77d3f2deef15
Kaloian Penchev <kaloanpencev8@gmail.com>
2025-05-17 13:36:23 +0300

    minimal change

--- sync/api/serializers.py
+++ sync/api/serializers.py
@@ -26,4 +26,12 @@ class CommentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Comment
         fields = ['id', 'user', 'post', 'content', 'created_at']
-        read_only_fields = ['created_at', 'user'] 
+        read_only_fields = ['created_at', 'user']
+
+class LikeSerializer(serializers.ModelSerializer):
+    user = UserSerializer(read_only=True)
+    
+    class Meta:
+        model = Like
+        fields = ['id', 'user', 'post', 'created_at']
+        read_only_fields = ['created_at', 'user', 'post'] 
