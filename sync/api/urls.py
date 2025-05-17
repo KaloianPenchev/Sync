91c7b0b31ae367d934240e45e98952ad48f3e61f
Kaloian Penchev <kaloanpencev8@gmail.com>
2025-05-17 13:36:17 +0300

    created url for the likes view

--- sync/api/urls.py
+++ sync/api/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path("posts/", views.post_list, name="post-list"),
     path("posts/<int:pk>/", views.post_detail, name="post-detail"),
+    path("posts/<int:pk>/like/", views.post_like, name="post-like"),
+    path("posts/<int:pk>/likes/", views.post_likes_list, name="post-likes-list"),
 ]
