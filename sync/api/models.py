dc63162bd3226f789d2efacc5187dd62ac1b3d50
Kaloian Penchev <kaloanpencev8@gmail.com>
2025-05-19 21:27:48 +0300

    created Follow model

--- sync/api/models.py
+++ sync/api/models.py
@@ -25,3 +25,14 @@ class Comment(models.Model):
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
+
+class Follow(models.Model):
+    follower = models.ForeignKey(User, related_name='following', on_delete=models.CASCADE)
+    followed = models.ForeignKey(User, related_name='followers', on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+    class Meta:
+        unique_together = ('follower', 'followed')
+        
+    def __str__(self):
+        return f"{self.follower.username} follows {self.followed.username}"
